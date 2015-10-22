2bf84684b4c2b6ca973e68c7bf788ab14b613418
David Gall <dlaker74@fullsail.edu>
2015-10-22 16:24:53 -0700

    Comments
    Commented on my code to explain the use and thought of each section.

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -13,18 +13,18 @@ class Blog extends CI_Controller {
         parent::__construct();
         $this->load->helper('form');
     }
-
+//This section is my initial page.
     public function index(){
-
+//Loads the url helper which makes it easy to build url request within your document.
         $this->load->helper('url');
-
+//This is the header view that is called with this code.
         $this->load->view('header');
-
+//Brings up the login/sign up form and loads it into view.
         $this->load->view('signup_login');
-
+//The footer of the code which just has closing divs, body and html tags.
         $this->load->view('footer');
     }
-
+//Sign up form which pulls the form that is needed to add a user.
     public function signup(){
 
         $this->load->helper('url');
@@ -38,10 +38,11 @@ class Blog extends CI_Controller {
 
 
     }
-
+//This is used to add a user to the database
     public function addUser(){
         $this->load->helper('url');
         $this->load->database();
+        //Creates an array that will hold the data entered by the user from the form.
         $data = array(
             'firstname'=>$this->input->post('firstName'),
             'lastname'=>$this->input->post('lastName'),
@@ -50,9 +51,9 @@ class Blog extends CI_Controller {
             'email'=>$this->input->post('email'),
 
         );
-
+//This sentence inserts the data into the database.
         $this->db->insert('users', $data);
-
+//This next section echos out the header instead of calling it because I needed the page to look a certain way.
         echo '<!DOCTYPE html>
 <html>
 <head lang="en">
@@ -82,10 +83,10 @@ class Blog extends CI_Controller {
             <p>Where would like to go?</p>
 <a href="http://localhost:8888/CodeIgniter-3.0.2/index.php/blog/searchPage/"><button type="submit" class="userButton">Search</button> </a>
 <a href="http://localhost:8888/CodeIgniter-3.0.2/index.php/blog/bac/"><button type="submit" class="userButton">BAC</button> </a>';
-
+//The urls for the buttons and links are using the url form to send the user where they need to go. Also for the stylesheets and scripts we use the full path to the folder.
         $this->load->view('footer');
     }
-
+//This function will grab a user by comparing the entered data against the database information.
     public function getUser()
     {
         $this->load->helper('url');
@@ -94,7 +95,7 @@ class Blog extends CI_Controller {
         $username = $this->input->post('username');
         $password = $this->input->post('password');
 
-//This is correct, I just need to figure out how to display the first name on the page.
+//This is the query that searches the database and finds the correct user to display.
 
         $this->db->select('*');
         $this->db->from('users');
@@ -139,7 +140,7 @@ class Blog extends CI_Controller {
         $this->load->view('footer');
 
     }
-
+//This is the delete function that will delete a user from the database if they so wish.
     public function deleteuser(){
         $this->load->helper('url');
         $this->load->database();
@@ -147,7 +148,7 @@ class Blog extends CI_Controller {
         $user_id = $this->input->post('user_id');
 
         $sql = ("DELETE FROM users WHERE user_id = $user_id");
-
+//This here is the query that will search and find the right user and delete their information.
         $this->load->view('header');
         $this->db->select('*');
         $this->db->from('users');
@@ -156,7 +157,7 @@ class Blog extends CI_Controller {
 
 
     }
-
+//This function will update a users information if they decide they want to change something.
     public function updateUser(){
         $this->load->helper('url');
         $this->load->database();
@@ -165,7 +166,7 @@ class Blog extends CI_Controller {
             echo $_SESSION['username'];
             unset($_SESSION['username']);
         }
-
+//This will load the view for the header and set the variable for user_id and then search for a match in the database.
         $this->load->view('header');
         $user_id = $this->input->post('user_id');
         $this->db->select('*');
@@ -177,8 +178,8 @@ class Blog extends CI_Controller {
 
 
 
-
-
+//Once a match is found you can grab the information using the foreach loop and displaying the information like shown below.
+//I added the user_id to the submit button so that it would carry over into the next page.
         foreach ($query->result() as $row)
         {
 
@@ -206,20 +207,20 @@ class Blog extends CI_Controller {
         $this->load->view('footer');
 
     }
-
+//This section completes the update for the users new information.
     public function completeUpdate(){
         $this->load->helper('url');
         $this->load->database();
 
         $user_id = $this->input->post('user_id');
-
+//Above sets the variable for the user_id and below are the variables that grab the input information.
         $fName = $this->input->post('firstName');
         $lName = $this->input->post('lastName');
         $uName = $this->input->post('username');
         $pass = $this->input->post('password');
         $ema = $this->input->post('email');
 
-
+//The query built to update the information in the users file.
         $array = array('firstname'=>$fName, 'lastname'=>$lName, 'username'=>$uName, 'password'=>$pass, 'email'=>$ema);
         $user = array('user_id'=>$user_id);
         $this->db->set($array);
@@ -228,7 +229,7 @@ class Blog extends CI_Controller {
 
         echo '<h1>Update Succesful!!</h1>';
     }
-
+//This page is the main search page for my app. It has two search possibilities, one for drinks and one by ingredient.
     public function searchPage(){
         $this->load->helper('url');
         $this->load->database();
@@ -237,13 +238,13 @@ class Blog extends CI_Controller {
         $this->load->view('search');
         $this->load->view('footer');
     }
-
+//This function is to display the results of a drink searched by the user.
     public function detailsPage(){
         $this->load->helper('url');
         $this->load->database();
 
         $drink = $this->input->post('drink');
-
+//Above will grab the name of the drink searched and set it to a variable and below will run the query to find that drink.
         $this->db->select('*');
         $this->db->from('drinks');
         $array = array('drink_name'=>$drink);
@@ -291,9 +292,12 @@ class Blog extends CI_Controller {
 
         </div>';
         }
+        //All the data is run through a foreach loop and then displayed appropriately.
         $this->load->view('footer');
     }
-
+//This page exists for those that decide to run a search for a drink with a specific ingredient.
+//It will return all the drinks with searched ingredient and place them into a clickable list.
+//The query in this function uses like to compare the searched ingredient because there wont be an exact match.
     public function resultsPage(){
         $this->load->helper('url');
         $this->load->database();
@@ -303,7 +307,6 @@ class Blog extends CI_Controller {
         $this->db->from('drinks');
         $this->db->where($sql);
         $query = $this->db->get();
-//This is also done I just need to figure out how to display the results where I want on the page.
 
         echo '<!DOCTYPE html>
 <html>
@@ -332,7 +335,7 @@ class Blog extends CI_Controller {
         <hr>
 <div class="col-md-6">
 ';
-
+//Using the foreach loop we can run through the results and place them in a button link. We also use the drink name and send it as a value so that we can grab it use it to query the database with.
         foreach ($query->result() as $row) {
 
             echo '<form action="http://localhost:8888/CodeIgniter-3.0.2/index.php/blog/detailsPage/" method="POST"> <button type="submit" name="drink" class="userButton" value="' . $row->drink_name .'">'.$row->drink_name.'</button>   </form>';
@@ -342,7 +345,7 @@ class Blog extends CI_Controller {
         $this->load->view('footer');
 
     }
-
+//This is my blood alcohol level calculator that uses different formulas to determine an approximate level of inebriation.
     public function bac(){
         $this->load->view('bac');
     }
